d0599c7ee945cc33024d6f9748eb6bb4f3ad9181
misakigr <misakigr@mail.ru>
2021-11-03 15:44:29 +0300

    исправили сортировку Input script

--- NEWblockchain-parser.py
+++ NEWblockchain-parser.py
@@ -53,9 +53,9 @@ def read_varint(file):
         data = b + data
     return data
 
-dirA = 'D:\Загрузки\lockchain\locks/' # Directory where blk*.dat files are stored
+dirA = 'D:\misak\lockchain\locks/' # Directory where blk*.dat files are stored
 #dirA = sys.argv[1]
-dirB = 'D:\Загрузки\lockchain/' # Directory where to save parsing results
+dirB = 'D:\misak\lockchain/' # Directory where to save parsing results
 #dirA = sys.argv[2]
 
 fList = os.listdir(dirA)
@@ -251,42 +251,11 @@ for i in fList:
         if tmpHex != MerkleRoot:
             print ('Merkle roots does not match! >',MerkleRoot,tmpHex)
     f.close()
-    # f = open(dirB + nameRes,'w')
-    # for j in resList:
-    #     f.write(j + '\n')
-    # f.close()
-    os.remove(t)
-    
-    
-# dirA = 'D:/misak/2/' # Directory where blk*.dat files are stored
-# dirB = 'D:/misak/1/' # Directory where to save parsing results
 
-# fList = os.listdir(dirA)
-# fList = [x for x in fList if (x.endswith('.txt') and x.startswith('blk'))]
-# fList.sort()
-
-# for i in fList:
-#     nameSrc = i
-#     nameRes = nameSrc.replace('.txt','.dat')
-    # resList = []
     a = 0
-    # t = dirA + nameSrc
-    # resList.append('Start ' + t + ' in ' + str(datetime.datetime.now()))
     print ('Обработка файла ' + t )
     n = 0
-    # with open(t, 'r+') as f:
-    #     f.writelines( ["\n"]+[f.readlines(),f.seek(0)][0] )
-    # with open(t, "r") as f, open("restest.txt", "w") as outfile:
-    #     for i in f.readlines():
-    
-    #         if not i.strip():
-    #             outfile.write('DateGroup' + str(n) + '\n')
-    #             n += 1
-    #             continue
-    #         if i:
-    #             outfile.write(i)
-    
-    
+
     DATE_GROUP_SEPARATOR = "DateGroup"
     sorted_data = {}
     
@@ -303,8 +272,8 @@ for i in fList:
         for date_group, dates in sorted_data.items():
             # print(f"{date_group}: {dates}")
             filehandle.writelines(place for place in (f"{date_group}: {dates}" + '\n'))
-    
-    
+
+
     word = 'Inputs count = 02'  # Искомое слово
     dorw = 'Outputs count = 1'  # Искомое слово 2
     resList = []
@@ -315,30 +284,33 @@ for i in fList:
                 data = text.split(", ")
                 for temp in data:
                     resList.append(temp)
-    
+
     f = open(dirA+nameSrc, 'w')
     for j in resList:
         f.write(j + '\n')
     f.close()
     resList = []
     os.replace(dirA + nameSrc, dirB + nameSrc)
-    
+
     word = 'Input script'  # Искомое слово
     with open(dirB+nameSrc) as file:
         for line in file:
             if word in line:
                 text = str(line)
-                b = text.partition('022')[2]
-                b = b.partition('022')[0]
-                if len(b) < 80 and len(b) > 60:
-                    resList.append(b[-64:]+'\n')
-    
-        
+                kb = (text.partition('Input script')[2])[:79]
+                print(kb)
+                if '0220' in kb:
+                    b = kb.partition('0220')[2]
+                    resList.append(b[:64]+'\n')
+                elif '022100' in kb:
+                    b = kb.partition('022100')[2]
+                    resList.append(b[:64] + '\n')
+
         f = open('restest.txt', 'w')
         for j in resList:
             f.write(j + '\n')
         f.close()
-    
+
         # убираем пустые строки и записываем в выходной файл
         with open("restest.txt", "r") as f, open("outfile.txt", "w") as outfile:
             for i in f.readlines():
@@ -347,10 +319,10 @@ for i in fList:
                 if i:
                     outfile.write(i)
         os.remove('restest.txt')
-        
-        
-    
-    
+
+
+
+
         from collections import defaultdict
         res = defaultdict(list)
         with open('outfile.txt') as infile:
@@ -370,7 +342,7 @@ for i in fList:
             res = []
     if a==0:
         os.remove(dirB+nameSrc)
-    a=0    
+    a=0
     
 
     
