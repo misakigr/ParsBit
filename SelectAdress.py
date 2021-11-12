dc9cada94f38c1cfefdff9231c9a2de7a99e057c
misakigr <misakigr@mail.ru>
2021-11-12 10:36:20 +0300

    Выборка адресов в один файл Lists.txt

---
+++ SelectAdress.py
@@ -0,0 +1,46 @@
+import os
+
+
+dirA = 'wal_adr/' # Directory where blk*.dat files are stored
+#dirA = sys.argv[1]
+dirB = 'SelectAdress/' # Directory where to save parsing results
+#dirA = sys.argv[2]
+
+fList = os.listdir(dirA)
+fList = [x for x in fList if (x.endswith('.dat') and x.startswith('list'))]
+fList.sort(reverse=True)
+#Источник: https://pythonim.ru/list/metod-sort-python)
+
+word = 'Public Address 1: '  # Искомое слово
+dorw = 'Public Address 1 compressed: '  # Искомое слово 2
+resList = []
+
+for i in fList:
+    nameSrc = i
+    nameRes = nameSrc.replace('.dat','.txt')
+    # resList = []
+    a = 0
+    t = dirA + nameSrc
+    # resList.append('Start ' + t + ' in ' + str(datetime.datetime.now()))
+    print ('Start ' + t )
+    f = open(t)
+    for line in f:
+        if word in line:
+            text = str(line)
+            resList.append(text.partition(word)[2])
+            # print(kb)
+        if dorw in line:
+            text = str(line)
+            resList.append(text.partition(dorw)[2])
+
+    # f = open(dirB + nameSrc, 'w')
+    # for j in resList:
+    #     f.write(j + '\n')
+    # f.close()
+    # resList = []
+
+f = open('Lists.txt', 'w')
+for j in resList:
+    f.write(j)
+f.close()
+resList = []
